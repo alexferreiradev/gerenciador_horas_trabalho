829b4c22285222bfdab1cc562b6da1ea2abd3f78
Alex Rabelo Ferreira <arf92@live.com>
2020-08-10 11:01:11 -0300

    Adicao de layouts para dashboard

--- frontend-web/src/pages/Dashboard/index.js
+++ frontend-web/src/pages/Dashboard/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container, Resumo, ListaLancamento } from './styles';
 
 function Dashboard() {
+  const [lancamentoList, setLancamentoList] = useState([
+    { id: 1, acao: 'teste' },
+  ]);
+
   return (
     <Container>
-      <Resumo>Resumo de dados</Resumo>
-      <ListaLancamento>Lista de lancamentos</ListaLancamento>
+      <Resumo>
+        <h1>Dashboard</h1>
+        <ul>
+          <li>
+            <span>Total horas:</span>
+            {8}h
+          </li>
+          <li>
+            <span>Total OS:</span>
+            {2}
+          </li>
+        </ul>
+      </Resumo>
+      <ListaLancamento>
+        <h1>Lista de lancamentos</h1>
+        {lancamentoList.lenght === 0 && <span>Não há lancamentos</span>}
+        {lancamentoList.map((lancamento) => (
+          <div key={lancamento.id}>
+            <span>15:00 - </span>
+            <span>16:00 - </span>
+            <span>1h - </span>
+            <span>OS#1234 - </span>
+            <span>{lancamento.acao}</span>
+          </div>
+        ))}
+      </ListaLancamento>
     </Container>
   );
 }
--- frontend-web/src/pages/Dashboard/styles.js
+++ frontend-web/src/pages/Dashboard/styles.js
@@ -6,15 +6,31 @@ export const Container = styled.div`
   background: #ccc;
   height: 100%;
   width: 100%;
+
+  h1 {
+    font-size: 14px;
+    align-self: center;
+    margin-bottom: 16px;
+  }
 `;
 
 export const Resumo = styled.div`
   display: flex;
+  flex-direction: column;
   width: 35%;
   height: 100%;
+  margin: 8px;
 `;
+
 export const ListaLancamento = styled.div`
   display: flex;
+  flex-direction: column;
   width: 75%;
   height: 100%;
+  margin: 8px;
+
+  div {
+    border: 1px solid #000;
+    padding: 4px;
+  }
 `;
