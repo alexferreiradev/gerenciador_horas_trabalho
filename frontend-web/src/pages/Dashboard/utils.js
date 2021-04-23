6efebfc2c704ef5a743eab37d166ff24b0750a3a
Alex Rabelo Ferreira <arf92@live.com>
2021-04-23 19:23:19 -0300

    add: metodos uteis para trabalhar com intervalo

--- frontend-web/src/pages/Dashboard/utils.js
+++ frontend-web/src/pages/Dashboard/utils.js
@@ -1,8 +1,52 @@
-import { convertMinutesToObj } from '../../util/index';
+import { convertMinutesToObj, formatHoraLancamento } from '../../util/index';
+
+function createTextoBotaoFrom(newLancamento) {
+  const { isIntervalo, tarefaEvolutiva } = newLancamento;
+  if (isIntervalo) {
+    return 'Bloquear';
+  }
+  if (tarefaEvolutiva) {
+    return 'Copy to Redmine';
+  }
+
+  return 'Copy to OS';
+}
 
 export function convertIntervaloParaTempo({ intervalo, tarefaEvolutiva }) {
   const { hora, minuto } = convertMinutesToObj(intervalo);
   const minutoScaled = `${(minuto / 60).toFixed(2)}`.split('.')[1];
   if (tarefaEvolutiva) return `${hora}.${minutoScaled}h`;
   return `${hora}h, ${minuto}m`;
+}
+
+export function filterLancamentosWithIntervalo(lancamentoList) {
+  return lancamentoList.filter((l) => !l.isIntervalo);
+}
+
+export function createAcaoFrom(newLancamento) {
+  if (newLancamento.isIntervalo) {
+    return 'Intervalo - Contabilizado somente na ultima hora lançada';
+  }
+
+  return newLancamento.acao;
+}
+
+export function createObjetoLancamentoFrom(newLancamento, lastId) {
+  const newModel = {
+    ...newLancamento,
+    hora: new Date(),
+    horaFormatted: formatHoraLancamento(new Date()),
+    minutesConverted: convertIntervaloParaTempo(newLancamento),
+    acao: createAcaoFrom(newLancamento),
+    textoBotaoAcao: createTextoBotaoFrom(newLancamento),
+  };
+
+  if (lastId) {
+    return {
+      ...newModel,
+      id: lastId + 1,
+    };
+  }
+
+  return newModel;
 }
