119a077094bc5c2550d60e16fb9387d2ac7741d3
Alex Rabelo Ferreira <arf92@live.com>
2021-04-23 18:27:44 -0300

    add: constantes em arquivo separado

---
+++ frontend-web/src/pages/Dashboard/Constantes.js
@@ -0,0 +1,12 @@
+const emptyLancamento = {
+  os: undefined,
+  acao: '',
+  intervalo: '',
+  sistema: undefined,
+  copied: false,
+  tarefaEvolutiva: false,
+};
+
+export default {
+  emptyLancamento,
+};
