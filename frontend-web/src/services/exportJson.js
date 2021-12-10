0eeb852c826ab019c9ea4031e8a12ea309989a1c
Alex-Ferreira <alex.ferreira@invillia.com>
2021-12-10 20:17:17 -0300

    #16 add: serviço de exportacao json

---
+++ frontend-web/src/services/exportJson.js
@@ -0,0 +1,27 @@
+import { format } from 'date-fns';
+
+function exportJSON(horaInicio, lancamentoList) {
+  const jsonToExport = {
+    horaInicio,
+    lancamentoList,
+  };
+
+  const blob = new Blob([JSON.stringify(jsonToExport, null, 2)], {
+    type: 'application/json',
+  });
+
+  const fileName = `horas_talento-${format(Date.now(), 'yyyy-MM-dd')}.json`;
+  const file = new File([blob], fileName);
+
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = window.URL.createObjectURL(file);
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(a.href);
+
+  return file;
+}
+
+export default exportJSON;
