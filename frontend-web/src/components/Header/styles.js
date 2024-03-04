5e52986accd6a7764d0f5927a374a313d15fd832
Alex-Ferreira <alex.ferreira@invillia.com>
2024-03-04 07:56:14 -0300

    update: Header with Icon
    - add Icon component
    - change style to new component

--- frontend-web/src/components/Header/styles.js
+++ frontend-web/src/components/Header/styles.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
-  margin: 50px 0;
+  margin: 50px 0px;
+  gap: 16px;
 
   span {
     color: #fff;
