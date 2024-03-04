472ac6c601ebe0458286bde1e7f1d1bd6cbd5bc6
Alex-Ferreira <alex.ferreira@invillia.com>
2024-03-04 07:55:01 -0300

    add: Icon component
    - it is the svg file converted using svg2jsx

---
+++ frontend-web/src/components/Header/Icon/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16mm"
+      height="60.472"
+      version="1.1"
+      viewBox="0 0 60.472 60.472"
+      xmlSpace="preserve"
+    >
+      <g transform="translate(-189.435 -1506.25)">
+        <g transform="matrix(.17632 0 0 .17632 493.04 1494.425)">
+          <g stroke="#000" transform="translate(78.627 -218.803)">
+            <circle
+              cx="-1629.069"
+              cy="457.358"
+              r="165.695"
+              fill="#fff"
+              strokeDasharray="none"
+              strokeWidth="11.584"
+            ></circle>
+            <path
+              fill="#000"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeOpacity="1"
+              strokeWidth="1"
+              d="M-1557.844 343.673l29.77 37.452-91.964 73.192-2.706-74.591 11.052.014 2.033 42.957 6.9.927.918-86.222-8.782-.66-1.175 31.654-11.307-1.464c-2.58-65.984-3.633-81.512-9.464-.056l-7.346-.332.141-30.062-11.426-.02.114 80.727 9.193-.044.983-37.143 7.092-.05-4.79 82.538 8.53 10.83 111.873-78.264 19.138 31.811 10.554-8.27-60.462-81.568z"
+            ></path>
+            <path
+              fill="#ccc"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeOpacity="1"
+              strokeWidth="1"
+              d="M-1619.565 383.585l11.051.014 2.033 42.957 6.9.928.918-86.223-8.782-.66-1.175 31.654-11.307-1.463-9.463-.056-7.347-.332.141-30.063-11.425-.02.113 80.728 9.194-.044.982-37.144 7.093-.05c3.525 1.097 11.38 3.454 11.074-.226z"
+            ></path>
+            <path
+              fill="#b3b3b3"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeOpacity="1"
+              strokeWidth="1"
+              d="M-1553.363 343.402l29.77 37.452-91.963 73.192-18.57 8.173 8.53 10.83 111.872-78.264 19.138 31.81 10.554-8.27-60.462-81.567z"
+            ></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default React.memo(Icon);
